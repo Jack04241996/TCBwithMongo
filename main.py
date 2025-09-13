703295428b8d14eb5c9992e4e59de56c366ef92e
Jack04241996 <jack1996042450806@gmail.com>
2025-09-13 17:11:09 +0800

    Test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
- #test2
+ #test1
 def _redact(uri: str) -> str:
     if not uri:
         return ""
